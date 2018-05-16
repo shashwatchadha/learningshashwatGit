e195dffce283a1356152aa0dc0c352f2d6a04d52
shashwatchadha <shashwatchadhaa@gmail.com>
2018-05-16 16:18:18 +0530

    first commit

---
+++ src/main/java/testJen/TEst.java
@@ -0,0 +1,26 @@
+package testJen;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TEst {
+
+	WebDriver driver;
+	
+	
+	@BeforeTest
+	public void beforetest()
+	{
+		driver = new FirefoxDriver();
+		driver.get("http://www.google.co.in");
+	}
+	
+	@Test
+	public void t1()
+	{
+		driver.findElement(By.xpath("//*[@value='Google Search']")).click();
+	}
+}
